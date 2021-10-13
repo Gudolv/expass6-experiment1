aff6006453d77cb23b489d4e3ca5c232de35cf1a
gudolv <gudolv@hotmail.com>
2021-10-13 15:29:14 +0200

    experiment done

---
+++ src/main/java/com/example/expass6experiment1/Expass6Experiment1Application.java
@@ -0,0 +1,13 @@
+package com.example.expass6experiment1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Expass6Experiment1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Expass6Experiment1Application.class, args);
+    }
+
+}
